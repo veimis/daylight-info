b118e0e989cf33831f80f24f64f95fa1a8b5497d
Ilkka Veima <ilkka.veima@leanware.fi>
2019-05-10 22:23:14 +0800

    Add summer time and normal time (winter) graphs.

--- index.js
+++ index.js
@@ -1,7 +1,63 @@
 // Depends on dataLoader.js, graph.js
 
+const start = new Date('2019-03-30');
+const end = new Date('2019-10-26');
+
 const data = dataLoader.load();
 
-const selection = d3.select('svg');
-graph.render(selection, data);
+const summerData = data.map(summerize);
+const summer = d3.select('#summer');
+graph.render(summer, summerData);
+
+const winterData = data.map(normalize);
+const winter = d3.select('#winter');
+graph.render(winter, winterData);
+
+function summerize(item) {
+	if(!isDaylightSavingDate(item)) {
+		return {
+			date: item.date,
+			rise: addOneHour(item.rise),
+			set: addOneHour(item.set)
+		};
+	}
+
+	return item;
+}
+
+function normalize(item) {
+	if(isDaylightSavingDate(item)) {
+		return {
+			date: item.date,
+			rise: removeOneHour(item.rise),
+			set: removeOneHour(item.set)
+		};
+	}
+	
+	return item;
+}
+
+function removeOneHour(time) {
+	const splitTime = time.split(':');
+	const hours = parseInt(splitTime[0]) - 1;
+	const minutes = parseInt(splitTime[1]);
+	
+	return toString(hours, minutes);
+}
+
+function addOneHour(time) {
+	const splitTime = time.split(':');
+	const hours = parseInt(splitTime[0]) + 1;
+	const minutes = parseInt(splitTime[1]);
+
+	return toString(hours, minutes);
+}
+
+function isDaylightSavingDate(item) {
+	return item.date > start && item.date < end;
+}
+
+function toString(hours, minutes) {
+	return hours.toString().padStart(2, '0') + ":" + minutes;
+}
 
