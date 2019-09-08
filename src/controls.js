99ba2a059c3db75780aee23b9b72c48b85e19ae1
Ilkka Veima <ilkka.veima@leanware.fi>
2019-09-08 15:20:22 +0800

    Add location input controls and fetch dummy results from example html page.
    Text input takes a name of a place and query is sent out.
    The resulting html is parsed for the names of the places and
    links for the 12 months daylight data in each place,
    which are then presented as a list.

---
+++ src/controls.js
@@ -0,0 +1,65 @@
+// Depends on ??
+
+// Tie form submit
+var controls = (function() {
+	return {
+
+		fetchPlaces: function() {
+			event && event.preventDefault();
+
+			const xhr = new XMLHttpRequest();
+			xhr.open('GET', 'locations.html');
+			xhr.responseType = 'document';
+			xhr.send();
+
+			xhr.onload = () => {
+				const places = parseData(xhr.response);
+				const results = createResultList(places);
+
+				document.getElementById('places').appendChild(results);
+			}
+
+			xhr.onerror = () => {
+				console.log(xhr);
+				console.log('Failed to fetch places');
+			}
+		}
+	}
+
+	function parseData(data) {
+		// HTMLCollection is converted to an array with [...HTMLCollection]
+
+		const tables = data.body.getElementsByTagName('table');
+
+		const tableRows = [...tables[2].tBodies[0].children]; 
+		tableRows.shift(); // Remove header row
+
+		return getPlaces(tableRows);
+	}
+
+	function getPlaces(tableRows) {
+		let places = [];
+
+		tableRows.forEach(row => {
+			const rowItems = [...row.children];
+			const name = rowItems[2].childNodes[0].nodeValue.trim();
+			const link = [...rowItems.pop().children].pop().href;
+			places.push({name, link});
+		});
+
+		return places;
+	}
+
+	function createResultList(places) {
+		let list = document.createElement('ul');
+
+		places.forEach(place => {
+			let listItem = document.createElement('li');
+			listItem.appendChild(document.createTextNode(place.name));
+			list.appendChild(listItem);
+		});
+
+		return list;
+	}
+
+}());
