d0de90570fdf5fae11b66d3ae83cca970078a388
Ilkka Veima <ilkka.veima@leanware.fi>
2019-09-14 14:17:45 +0800

    Use regex capture groups instead of manual parsing.

--- src/dataLoader.js
+++ src/dataLoader.js
@@ -2,7 +2,7 @@
 
 // Create dataLoader object to global scope
 var dataLoader = (function(){
-	const re = /\d{1,2}\.\d{1,2}\.\d{4}( - \d{1,2}:\d{1,2}){2}/g; // 23.4.2019 - 5:40 - 21:09
+	const re = /(\d{1,2}\.\d{1,2}\.\d{4}) - (\d{1,2}:\d{1,2}) - (\d{1,2}:\d{1,2})/g; // 23.4.2019 - 5:40 - 21:09
 	const d3DateParser = d3.timeParse("%d.%m.%Y");
 
 	return {
@@ -25,36 +25,26 @@ var dataLoader = (function(){
 		}
 	}
 
+	// Recursively parse data
 	function parseData(data, results) {
 		let match = re.exec(data);
 		if(!match){
 			return results;
 		}
 
-		const parts = parseItem(match[0]);
-
-		const date = parseDate(parts[0]);
+		const date = d3DateParser(match[1].trim());
 		results.dates.push(date);
 
-		const dataElement = newDataElement(parts);
+		const dataElement = newDataElement(match[2], match[3]);
 		results.values.push(dataElement);
 		
 		return parseData(data, results);
 	};
 
-	function parseItem(line) {
-		return line.split('-');
-	};
-	
-	function parseDate(date) {
-		return d3DateParser(date.trim());
-	}
-
-	// Data element
-	function newDataElement(dataRow) {
+	function newDataElement(riseTime, setTime) {
 		return {
-			rise:	toSeconds(dataRow[1].trim()),
-			set: toSeconds(dataRow[2].trim())
+			rise:	toSeconds(riseTime.trim()),
+			set: toSeconds(setTime.trim())
 		}
 	}
 
